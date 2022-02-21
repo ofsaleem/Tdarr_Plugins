6ca34fd3b69bd0edf0083c697a4b18062041872b
Omar Saleem <ofsaleem@users.noreply.github.com>
2022-02-21 05:24:18 -0500

    attempting to fix indentation

--- Community/Tdarr_Plugin_zeroPraxis_filter_by_chroma.js
+++ Community/Tdarr_Plugin_zeroPraxis_filter_by_chroma.js
@@ -1,79 +1,79 @@
 const details = () => ({
-    id: 'Tdarr_Plugin_zeroPraxis_filter_by_chroma',
-    Stage: 'Pre-processing',
-    Name: 'Filter by chroma scheme',
-    Type: 'Video',
-    Operation: 'Filter',
-    Description: 'Only allow specified chroma subsampling (4:4:4, 4:2:0, etc) scheme to be processed \n\n',
-    Version: '1.00',
-    Tags: 'filter',
-    Inputs: [
-      {
-        name: 'chromasToProcess',
-        type: 'string',
-        defaultValue: '',
-        inputUI: {
-          type: 'text',
-        },
-        tooltip:
-          'Enter a comma separated list of chroma subsampling schemes to be processed. Leave blank if using chromasToNotProcess',
+  id: 'Tdarr_Plugin_zeroPraxis_filter_by_chroma',
+  Stage: 'Pre-processing',
+  Name: 'Filter by chroma scheme',
+  Type: 'Video',
+  Operation: 'Filter',
+  Description: 'Only allow specified chroma subsampling (4:4:4, 4:2:0, etc) scheme to be processed \n\n',
+  Version: '1.00',
+  Tags: 'filter',
+  Inputs: [
+    {
+      name: 'chromasToProcess',
+      type: 'string',
+      defaultValue: '',
+      inputUI: {
+        type: 'text',
       },
-      {
-        name: 'chromasToNotProcess',
-        type: 'string',
-        defaultValue: '',
-        inputUI: {
-          type: 'text',
-        },
-        tooltip:
-          'Enter a comma separated list of chroma subsampling schemes to be not be processed. Leave blank if using chromasToProcess',
+      tooltip:
+        'Enter a comma separated list of chroma subsampling schemes to be processed. Leave blank if using chromasToNotProcess',
+    },
+    {
+      name: 'chromasToNotProcess',
+      type: 'string',
+      defaultValue: '',
+      inputUI: {
+        type: 'text',
       },
-    ],
-  });
+      tooltip:
+        'Enter a comma separated list of chroma subsampling schemes to be not be processed. Leave blank if using chromasToProcess',
+    },
+  ],
+});
   
-  // eslint-disable-next-line no-unused-vars
-  const plugin = (file, librarySettings, inputs, otherArguments) => {
-    const lib = require('../methods/lib')();
-    // eslint-disable-next-line no-unused-vars,no-param-reassign
-    inputs = lib.loadDefaultValues(inputs, details);
-    const response = {
-      processFile: false,
-      infoLog: '',
-    };
-  
-    const fileScheme = '';
-    for track in file.mediaInfo.track {
-        if track.@type == "Video" {
-            fileScheme = track.ChromaSubsampling;
-            break;
-        }
-    }
-  
-    if (inputs.chromasToProcess !== '') {
-      const schemes = inputs.chromasToProcess.split(',');
-      if (schemes.includes(fileScheme)) {
-        response.processFile = true;
-        response.infoLog += 'File is in chromasToProcess. Moving to next plugin.';
-      } else {
-        response.processFile = false;
-        response.infoLog += 'File is not in chromasToProcess. Breaking out of plugin stack.';
-      }
-    }
-  
-    if (inputs.chromasToNotProcess !== '') {
-      const schemes = inputs.chromasToNotProcess.split(',');
-      if (schemes.includes(fileScheme)) {
-        response.processFile = false;
-        response.infoLog += 'File is in chromasToNotProcess. Breaking out of plugin stack.';
-      } else {
-        response.processFile = true;
-        response.infoLog += 'File is not in chromasToNotProcess. Moving to next plugin.';
-      }
-    }
-  
-    return response;
+// eslint-disable-next-line no-unused-vars
+const plugin = (file, librarySettings, inputs, otherArguments) => {
+  const lib = require('../methods/lib')();
+  // eslint-disable-next-line no-unused-vars,no-param-reassign
+  inputs = lib.loadDefaultValues(inputs, details);
+  const response = {
+    processFile: false,
+    infoLog: '',
   };
-  
-  module.exports.details = details;
-  module.exports.plugin = plugin;
+
+  const fileScheme = '';
+  for track in file.mediaInfo.track {
+      if track.type == "Video" {
+          fileScheme = track.ChromaSubsampling;
+          break;
+      }
+  }
+
+  if (inputs.chromasToProcess !== '') {
+    const schemes = inputs.chromasToProcess.split(',');
+    if (schemes.includes(fileScheme)) {
+      response.processFile = true;
+      response.infoLog += 'File is in chromasToProcess. Moving to next plugin.';
+    } else {
+      response.processFile = false;
+      response.infoLog += 'File is not in chromasToProcess. Breaking out of plugin stack.';
+    }
+  }
+
+  if (inputs.chromasToNotProcess !== '') {
+    const schemes = inputs.chromasToNotProcess.split(',');
+    if (schemes.includes(fileScheme)) {
+      response.processFile = false;
+      response.infoLog += 'File is in chromasToNotProcess. Breaking out of plugin stack.';
+    } else {
+      response.processFile = true;
+      response.infoLog += 'File is not in chromasToNotProcess. Moving to next plugin.';
+    }
+  }
+
+  return response;
+};
+
+module.exports.details = details;
+module.exports.plugin = plugin;
   
